8e836ba407324799fbbbca16a7a5235f98067bee
rabbani4443 <rabbani07r@gmail.com>
2023-09-10 13:11:44 +0600

    the visited button is almost done

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -4,18 +4,54 @@ import Country from "../Country/Country";
 
 const Countries = () => {
     const [countries, setCountries]=  useState([]);
+
+    const [visitedCountries ,setVisitedCountries ] = useState([]);
+
+    const [visitedFlags , setVisitedFlags] = useState([]);
+
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res=>res.json())
         .then(data => setCountries(data))
-        
     },[])
+
+    const handelVisitedCountries =(country)=>{
+        const newVisitedCountries = [...visitedCountries, country]
+        setVisitedCountries(newVisitedCountries)
+    };
+
+    const handelVisitedFlags =(country) => {
+        const newVisitedFlags =[...visitedFlags, country];
+        setVisitedFlags(newVisitedFlags);
+    }
+
     return (
         <div className="w-11/12 mx-auto">
-            <h1 className='text-5xl font-bold text-center pb-10 pt-4'>Countries : {countries.length} </h1>
+            <h1 className='text-5xl font-bold text-center  pt-4'>Countries : {countries.length} </h1>
+            <div >
+            <h1 className='text-3xl font-bold text-center pb-10 pt-4'> Visited Countries : {visitedCountries.length} </h1>
+            <div className="grid lg:grid-cols-7 md:grid-cols-5 grid-cols-3 pb-10 gap-3 text-center">
+
+            {
+                visitedCountries.map(country=> <p key={country.ccn3} className=' font-semibold border border-purple-500 rounded-lg px-3 py-1 '> {country.name.common} </p>)
+            }
+            {
+               visitedFlags.map(country => <div key={country.ccn3} className='bg-slate-100 rounded-lg p-2'>
+                <p key={country.ccn3} className=' bg-gray-50 font-semibold  rounded-lg px-3 py-1 '> {country.name.common} </p>
+                <img className='h-20 w-full rounded-lg bg-slate-200 p-1' src={country.flags.png} />  
+                </div>) 
+            }
+
+            </div>
+            </div>
             <div className=" grid lg:grid-cols-4 md:grid-cols-3 ">
                 {
-                countries.map(country => <Country key={country.ccn3} country={country}></Country>)
+                countries.map(country => <Country 
+                    key={country.ccn3} 
+                    country={country} 
+                    handelVisitedCountries={handelVisitedCountries}
+                    handelVisitedFlags={handelVisitedFlags}
+                    ></Country>)
                 }
             </div>
         </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,17 +1,47 @@
+import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
+const Country = ({country, handelVisitedCountries,handelVisitedFlags}) => {
     const {name, flags,population,area,ccn3} = country;
+
+    const [visited, setVisited] = useState(false);
+
+    const handelVisited = () =>{
+        // setVisited(true)
+        setVisited(!visited)
+    }
+
+    // const passWithParents = () =>{
+    //     handelVisitedCountries(country)
+    // }
+
+    // const passWithParents = () => handelVisitedCountries(country)
+
     return (
-        <div className="country">
-            <h3 className='text-xl font-bold text-center bg-purple-600 py-1 rounded-lg	text-gray-100 mb-2'> {name.common}</h3>
+        <>
+        <div className={`country ${visited ? 'bg-gray-200' : 'bg-gray-50'}`}>
+            <h3 className= {`text-xl font-bold text-center text-purple-600 rounded-lg mb-2 ${visited && 'text-fuchsia-700'}`} > {name.common}</h3>
             <div className='bg-slate-400 rounded-lg p-1'>
             <img className='h-44 w-full rounded-lg ' src={flags.png} alt={`Flag of ${name.common}`} />
             </div>
             <p className=' font-semibold '> Country Code :{population} </p>
             <p className=' font-semibold '> Area :{area} </p>
             <p className=' font-semibold '> CCN3 :{ccn3} </p>
-            <button className='rounded-lg font-semibold bg-violet-700 py-1 px-5 hover:bg-violet-500 text-gray-100 '>Visited</button>
+            <div className='py-4 text-center'>
+            {/* <button onClick={passWithParents}  className= 'mr-2 rounded-lg font-semibold bg-violet-700 py-1 px-5 text-gray-100'> Mark Visited </button> */}
+            <button onClick={() => handelVisitedCountries(country)}  className= 'mr-2 rounded-lg font-semibold bg-violet-700 py-1 px-5 text-gray-100'> Mark Visited </button>
+
+            <button onClick={() => handelVisitedFlags(country)}  className= 'mr-2 rounded-lg font-semibold bg-fuchsia-700 py-1 px-5 text-gray-100'> Mark Flags </button>
+
+            <button onClick={handelVisited} className={`mb-3 rounded-lg font-semibold bg-violet-700 py-1 px-5 text-gray-100 ${visited && 'bg-fuchsia-700'}`}> {visited? 'Visited' : 'Going'}  </button> <br />
+            {/* {
+                visited && 'I have visited this country'
+            } */}
+            {
+                visited ? 'I have Visited this country' : 'I want to  visited'
+            }
+            </div>
         </div>
+    </>      
     );
 };
   export default Country;
